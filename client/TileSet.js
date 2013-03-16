d017b00a7e04ca396367d1d96d1244360e2e19d8
Charles Holbrow <charlesholbrow@gmail.com>
2013-03-16 18:45:50 -0400

    switching tab characters to two spaces

--- client/TileSet.js
+++ client/TileSet.js
@@ -4,34 +4,34 @@
 
 var TileSet = function(width, height, tileWidth, tileHeight, cellWidth, cellHeight, validIndexes) 
 {
-	this.width = width; // number tiles wide
-	this.height = height; // number tiles tall
-	this.tileWidth = tileWidth;
-	this.tileHeight = tileHeight;
-	this.cellWidth = cellWidth || tileWidth;
-	this.cellHeight = cellHeight || tileHeight;
-	this.length = width * height;
-	this.validIndexes = validIndexes || new Array(this.length);
-	if (!validIndexes) {
-	  for (var i=0; i<this.length; i ++){
-	    this.validIndexes[i] = i;
-	  };
-	};
+  this.width = width; // number tiles wide
+  this.height = height; // number tiles tall
+  this.tileWidth = tileWidth;
+  this.tileHeight = tileHeight;
+  this.cellWidth = cellWidth || tileWidth;
+  this.cellHeight = cellHeight || tileHeight;
+  this.length = width * height;
+  this.validIndexes = validIndexes || new Array(this.length);
+  if (!validIndexes) {
+    for (var i=0; i<this.length; i ++){
+      this.validIndexes[i] = i;
+    };
+  };
 };
 
 TileSet.prototype = {
-	getUpperLeftX: function(i) {
-		return ((i % this.width) * this.cellWidth) + 1;
-	},
+  getUpperLeftX: function(i) {
+    return ((i % this.width) * this.cellWidth) + 1;
+  },
 
-	getUpperLeftY: function(i) {
-		return (Math.floor(i / this.width) * this.cellHeight) + 1;
-	},
+  getUpperLeftY: function(i) {
+    return (Math.floor(i / this.width) * this.cellHeight) + 1;
+  },
 
-	loadImage: function(src, onload) {
-		this.src = src
-		this.image = new Image();
-		this.image.onload = onload;
-		this.image.src = src;
-	}
+  loadImage: function(src, onload) {
+    this.src = src
+    this.image = new Image();
+    this.image.onload = onload;
+    this.image.src = src;
+  }
 };
