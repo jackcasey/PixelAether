311a852c966f73ad1f663c91b7f19bef4e03687b
Charles Holbrow <charlesholbrow@gmail.com>
2014-01-12 12:37:08 -0500

    Input.drag(keyCode) listens for distance in addition to down duration

--- client/core/Input.js
+++ client/core/Input.js
@@ -1,26 +1,21 @@
-Beautiful.Input = function() {
+var InputState = function() {
+  this.downTime = new Date(0);
+  this.downFrameCount = -2;
+  this.downSimPos = {x:0, y:0};
+  this.upTime = new Date(1);
+  this.upFrameCount = -1;
+  this.upSimPos = {x:0, y:0};
+}
 
-  // ascii key codes to strings representing input gestures? or Actions?
+Beautiful.Input = function() {
 
   // States
   // Keycodes mapped to key state
-  // keys: keyCode number
-  // values: "States" - JS objects with the format: {
-  //      downTime 
-  //      downFrameCount 
-  //      upTime
-  //      upFrameCountCount
-  // }
   this.states = {};
 
   for (key in this.KEY){
     var keyCode = this.KEY[key];
-    this.states[keyCode] = {
-      downTime: new Date(0),
-      downFrameCount: -2,
-      upTime: new Date(1),
-      upFrameCount: -1,
-    };
+    this.states[keyCode] = new InputState;
   }
 
   // always keep track of mouse movement
@@ -32,13 +27,15 @@ Beautiful.Input = function() {
   };
 };
 
-
 /*------------------------------------------------------------
-bind
 drag
 hold
 isDown
 tap
+up
+
+TAP_THRESH
+DRAG_THRESH
 
 _keyDown
 _keyUp
@@ -46,16 +43,22 @@ _mouseDown
 _mouseMove
 _mouseUp
 
-TAP_THRESH
 KEY
 ------------------------------------------------------------*/
 Beautiful.Input.prototype = {
+
+TAP_THRESH: 200, // in millisec
+DRAG_THRESH: 15, // in pixels
 
 drag: function(keyCode) {
   var state = this.states[keyCode];
   if (!state) return null;
 
-  if (this.hold(keyCode) && // the action is holding
+  var deltaX = this.mouse.simPos.x - state.downSimPos.x;
+  var deltaY = this.mouse.simPos.y - state.downSimPos.y;
+  var hyp = hypotenuse(deltaX, deltaY);
+
+  if ( (this.hold(keyCode) || (this.isDown(keyCode) && hyp >= this.DRAG_THRESH)) && // the action is holding, or we moved with this mouse down
     this.mouse.moveFrameCount === gGame.simulation.frameCount) // mouse moved this frame
     return true;
 
@@ -126,6 +129,8 @@ _keyDown: function(keyCode, event) {
   var sim = gGame.simulation;
   state.downTime = sim.frameTime;
   state.downFrameCount = sim.frameCount;
+  state.downSimPos.x = this.mouse.simPos.x;
+  state.downSimPos.y = this.mouse.simPos.y;
 },
 
 _keyUp: function(keyCode, event) {
@@ -139,6 +144,8 @@ _keyUp: function(keyCode, event) {
   var sim = gGame.simulation;
   state.upTime = sim.frameTime;
   state.upFrameCount = sim.frameCount;
+  state.upSimPos.x = this.mouse.simPos.x;
+  state.upSimPos.y = this.mouse.simPos.y;
 },
 
 _mouseDown: function(event) {
@@ -160,8 +167,6 @@ _mouseUp: function(event) {
   var keyCode= (event.button === 0) ? this.KEY.MOUSE1 : this.KEY.MOUSE2;
   this._keyUp(keyCode, event);
 },
-
-TAP_THRESH: 200, // in milisec
 
 KEY: {
   'MOUSE1': -1,
@@ -256,100 +261,100 @@ KEY: {
   'COMMA': 188,
   'MINUS': 189,
   'PERIOD': 190
-},
-
-ASCII: {
-  "8": "BACKSPACE",
-  "9": "TAB",
-  "13": "ENTER",
-  "16": "SHIFT",
-  "17": "CTRL",
-  "18": "ALT",
-  "19": "PAUSE",
-  "20": "CAPS",
-  "27": "ESC",
-  "32": "SPACE",
-  "33": "PAGE_UP",
-  "34": "PAGE_DOWN",
-  "35": "END",
-  "36": "HOME",
-  "37": "LEFT_ARROW",
-  "38": "UP_ARROW",
-  "39": "RIGHT_ARROW",
-  "40": "DOWN_ARROW",
-  "45": "INSERT",
-  "46": "DELETE",
-  "48": "0",
-  "49": "1",
-  "50": "2",
-  "51": "3",
-  "52": "4",
-  "53": "5",
-  "54": "6",
-  "55": "7",
-  "56": "8",
-  "57": "9",
-  "65": "A",
-  "66": "B",
-  "67": "C",
-  "68": "D",
-  "69": "E",
-  "70": "F",
-  "71": "G",
-  "72": "H",
-  "73": "I",
-  "74": "J",
-  "75": "K",
-  "76": "L",
-  "77": "M",
-  "78": "N",
-  "79": "O",
-  "80": "P",
-  "81": "Q",
-  "82": "R",
-  "83": "S",
-  "84": "T",
-  "85": "U",
-  "86": "V",
-  "87": "W",
-  "88": "X",
-  "89": "Y",
-  "90": "Z",
-  "96": "NUMPAD_0",
-  "97": "NUMPAD_1",
-  "98": "NUMPAD_2",
-  "99": "NUMPAD_3",
-  "100": "NUMPAD_4",
-  "101": "NUMPAD_5",
-  "102": "NUMPAD_6",
-  "103": "NUMPAD_7",
-  "104": "NUMPAD_8",
-  "105": "NUMPAD_9",
-  "106": "MULTIPLY",
-  "107": "ADD",
-  "109": "SUBSTRACT",
-  "110": "DECIMAL",
-  "111": "DIVIDE",
-  "112": "F1",
-  "113": "F2",
-  "114": "F3",
-  "115": "F4",
-  "116": "F5",
-  "117": "F6",
-  "118": "F7",
-  "119": "F8",
-  "120": "F9",
-  "121": "F10",
-  "122": "F11",
-  "123": "F12",
-  "187": "PLUS",
-  "188": "COMMA",
-  "189": "MINUS",
-  "190": "PERIOD",
-  "function () { return \"function \" + name + \"() { [Command Line API] }\"; }": "toString",
-  "-1": "MOUSE1",
-  "-3": "MOUSE2"
 }
+
+// ASCII: {
+//   "8": "BACKSPACE",
+//   "9": "TAB",
+//   "13": "ENTER",
+//   "16": "SHIFT",
+//   "17": "CTRL",
+//   "18": "ALT",
+//   "19": "PAUSE",
+//   "20": "CAPS",
+//   "27": "ESC",
+//   "32": "SPACE",
+//   "33": "PAGE_UP",
+//   "34": "PAGE_DOWN",
+//   "35": "END",
+//   "36": "HOME",
+//   "37": "LEFT_ARROW",
+//   "38": "UP_ARROW",
+//   "39": "RIGHT_ARROW",
+//   "40": "DOWN_ARROW",
+//   "45": "INSERT",
+//   "46": "DELETE",
+//   "48": "0",
+//   "49": "1",
+//   "50": "2",
+//   "51": "3",
+//   "52": "4",
+//   "53": "5",
+//   "54": "6",
+//   "55": "7",
+//   "56": "8",
+//   "57": "9",
+//   "65": "A",
+//   "66": "B",
+//   "67": "C",
+//   "68": "D",
+//   "69": "E",
+//   "70": "F",
+//   "71": "G",
+//   "72": "H",
+//   "73": "I",
+//   "74": "J",
+//   "75": "K",
+//   "76": "L",
+//   "77": "M",
+//   "78": "N",
+//   "79": "O",
+//   "80": "P",
+//   "81": "Q",
+//   "82": "R",
+//   "83": "S",
+//   "84": "T",
+//   "85": "U",
+//   "86": "V",
+//   "87": "W",
+//   "88": "X",
+//   "89": "Y",
+//   "90": "Z",
+//   "96": "NUMPAD_0",
+//   "97": "NUMPAD_1",
+//   "98": "NUMPAD_2",
+//   "99": "NUMPAD_3",
+//   "100": "NUMPAD_4",
+//   "101": "NUMPAD_5",
+//   "102": "NUMPAD_6",
+//   "103": "NUMPAD_7",
+//   "104": "NUMPAD_8",
+//   "105": "NUMPAD_9",
+//   "106": "MULTIPLY",
+//   "107": "ADD",
+//   "109": "SUBSTRACT",
+//   "110": "DECIMAL",
+//   "111": "DIVIDE",
+//   "112": "F1",
+//   "113": "F2",
+//   "114": "F3",
+//   "115": "F4",
+//   "116": "F5",
+//   "117": "F6",
+//   "118": "F7",
+//   "119": "F8",
+//   "120": "F9",
+//   "121": "F10",
+//   "122": "F11",
+//   "123": "F12",
+//   "187": "PLUS",
+//   "188": "COMMA",
+//   "189": "MINUS",
+//   "190": "PERIOD",
+//   "function () { return \"function \" + name + \"() { [Command Line API] }\"; }": "toString",
+//   "-1": "MOUSE1",
+//   "-3": "MOUSE2"
+// }
 
 
 }; // Beautiful.Input.prototype
