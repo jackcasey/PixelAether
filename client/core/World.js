320b110317b601219a2eae0319edb91c14c007ed
Charles Holbrow <charlesholbrow@gmail.com>
2014-04-27 15:45:38 -0400

    Beautiful.World tracks servers, chunks, connections

--- client/core/World.js
+++ client/core/World.js
@@ -7,13 +7,98 @@ Beautiful.World = function(){
   this.view = new Beautiful.View(); // wraps our DOM canvas
   this.view.size.set(windowSize.width, windowSize.height);
   this.perspective = new Beautiful.Perspective(this.view); // Wraps chunkRenderers
+
+  this._portal = new Portal();  // current portal
+  this._portals = {};           // all portals 
+  this._portals[this._portal.url] = this._portal;
+
+  // Connection, server, chunks
+  this._portalDep = new Deps.Dependency;
 }
 
 /*------------------------------------------------------------
+world.perspective currently stores the map.tileset accessible via
+setters and getters. Is this the best place?
+
+For ChunkRenderer to get the correct data+tileset we need:
+  URL to the connected server
+  The connection object returned by DDP.connect
+    - will be passed to new Meteor.Collection as an option
+    - we call connection.subscribe to subscribe
+  Chunks collection from the connected server
+
+We also may need to re-think ChunkAddress - the autorun 
+function inside ChunkRenderer's initiazlier can get the 
+Chunks collection, and then the renderer can use the newly 
+retrived Collection object's .find instead of the global 
+variable
+
+connect
+getChunks
+getConnection
+getUrl
+go
 ------------------------------------------------------------*/
 Beautiful.World.prototype = {
-  // 
+
+  connect: function(url){
+    if (url === this._portal.url)
+      // already connecting/connected
+      return;
+
+    if (this._portals[url]){
+      // we have previously connected to this server
+      this._portal = this._portals[url];
+    }
+
+    else {
+      // first time connecting
+      this._portal = new Portal(url);
+      this._portals[url] = this._portal;
+      this._portal.getCollection('chunks');
+    }
+
+    this._portalDep.changed();
+  },
+
+  getChunks: function(){
+    this._portalDep.depend();
+    return this._portal.getCollection('chunks');
+  },
+
+  getConnection: function(){
+    this._portalDep.depend();
+    return this._portal.connection;
+  },
+
+  getUrl: function(){
+    this._portalDep.depend();
+    return this._portal.url;
+  },
+
   go: function(options){
-    
+    // see coords.md docs for details
   }
+
 } // Beautiful.World.prototype
+
+
+/*------------------------------------------------------------
+------------------------------------------------------------*/
+var Portal = function(url){
+  this.collections = {};
+  this.connection = url ? DDP.connect(url) : Meteor;
+  this.url = url || Meteor.absoluteUrl();
+}
+
+Portal.prototype = {
+
+  getCollection: function(name){
+    if (this.url === Meteor.absoluteUrl() && name === 'chunks')
+      return Chunks;
+    if (!this.collections[name])
+      this.collections[name] = new Meteor.Collection(name, this.connection);
+    return this.collections[name]
+  }
+
+}; // Portal.prototype
